7cff8905094ae11dc852ec79dc90ce5b80798f78
TaniachiFractal <tnmslv@yandex.ru>
2024-12-08 21:54:09 +0300

    Working on table config

---
+++ InvoiceSystem.Models.Configuration/CustomerConfig.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceSystem.Models.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceSystem.Models.Configuration
+{
+    /// <summary>
+    /// Конфигурация таблицы <see cref="Customer"/>
+    /// </summary>
+    public class CustomerConfig : IEntityTypeConfiguration<Customer>
+    {
+        void IEntityTypeConfiguration<Customer>.Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable($"{nameof(Customer)}s");
+        }
+    }
+}
